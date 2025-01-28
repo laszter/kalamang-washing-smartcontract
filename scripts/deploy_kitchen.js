a92e526bdef61791e09d7ed652b93108f5b60614
laszter <zeroasd1114@outlook.com>
2025-01-28 09:03:36 +0700

    edit address for mainnet

--- scripts/deploy_kitchen.js
+++ scripts/deploy_kitchen.js
@@ -9,10 +9,10 @@ async function main() {
         deployer.address
     );
 
-    const kycBitkubChainAddress = '0x99166455989a868d5151799c716B3c1Be95D5114';
-    const sdkTransferRouterAddress = '0xAE7D33f10f09669A86e45BAA6342377aFf4cF728';
-    const kkubAddress = '0x1BbE34CF9fd2E0669deEE34c68282ec1e6c44ab0';
-    const sdkCallHelperRouterAddress = '0x96f4C25E4fEB02c8BCbAdb80d0088E0112F728Bc';
+    const kycBitkubChainAddress = '0x409CF41ee862Df7024f289E9F2Ea2F5d0D7f3eb4';
+    const sdkTransferRouterAddress = ethers.ZeroAddress;
+    const kkubAddress = '0x67eBD850304c70d983B2d1b93ea79c7CD6c3F6b5';
+    const sdkCallHelperRouterAddress = ethers.ZeroAddress;
 
     const KalamangFeeStorage = await ethers.getContractFactory("KalamangFeeStorage");
     const feeContract = await KalamangFeeStorage.deploy();
